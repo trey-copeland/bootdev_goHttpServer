a3b32fd06e11555733777e5ed9275c7aa4410a60
Trey Copeland <jcopetre@gmail.com>
2026-02-25 22:57:50 -0600

    includes basic server instantiation

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	// "fmt"
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Boop")
+
+	mux := &http.ServeMux{}
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Fprintf(os.Stderr, "Server error: %v\n", err)
+	}
+
 }
