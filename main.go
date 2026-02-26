ae863ced7a389cc8dca8f607f16714add673721a
Trey Copeland <jcopetre@gmail.com>
2026-02-25 23:44:38 -0600

    adds basic file server

--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-	// "fmt"
 	"fmt"
 	"net/http"
 	"os"
 )
 
 func main() {
-
 	mux := &http.ServeMux{}
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		fmt.Fprintf(os.Stderr, "Server error: %v\n", err)
 	}
 
